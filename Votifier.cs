1d6f5a8c2097aeb7f12f49b35548e96495aefac9
fr34kyn01535 <fr34kyn01535@bam.yt>
2014-12-27 15:26:53 +0100

    Login notification

--- Votifier.cs
+++ Votifier.cs
@@ -8,6 +8,7 @@ using System.Timers;
 using System.IO;
 using System.Reflection;
 using Rocket.RocketAPI;
+using System.Net;
 
 namespace unturned.ROCKS.Votifier
 {
@@ -20,14 +21,53 @@ namespace unturned.ROCKS.Votifier
             try
             {
                 Configuration = (Configuration)(new Configuration()).LoadConfiguration();
+                if (!Configuration.Enabled) return;
                 Commands.RegisterCommand(new CommandReward("reward"));
                 Commands.RegisterCommand(new CommandReward("vote"));
+                SDG.Steam.serverConnected += checkVote;
             }
             catch (Exception ex)
             {
                 Logger.Log(ex.ToString());
             }
         }
+
+        public void checkVote(CSteamID id)
+        {
+            String result = new WebClient().DownloadString(String.Format("http://unturned-servers.net/api/?object=votes&element=claim&key={0}&steamid={1}", Votifier.Configuration.ServerKey, id.ToString()));
+            if (result == "1")
+            {
+                SteamPlayer steamPlayer = null;
+                SteamPlayerlist.tryGetSteamPlayer(id.ToString(), out steamPlayer);
+                string playerName = steamPlayer.Player.name;
+                ChatManager.say(String.Format("{0} voted for this server on unturned-servers.net and got a reward.", playerName));
+
+                bool success = true;
+
+                foreach (Reward reward in Votifier.Configuration.Rewards)
+                {
+                    if (!ItemTool.tryForceGiveItem(steamPlayer.Player, reward.ItemId, reward.Amount))
+                    {
+                        success = false;
+                    }
+                }
+
+                if (success)
+                {
+                    new WebClient().DownloadString(String.Format("http://unturned-servers.net/api/?action=post&object=votes&element=claim&key={0}&steamid={1}", Votifier.Configuration.ServerKey, id.ToString()));
+                }
+                else
+                {
+                    Logger.Log(String.Format("Failed giving a item to {0}", playerName));
+                }
+
+            }
+            else
+            {
+                ChatManager.say(id, "Vote for this server on unturned-servers.net and get a reward! Type /reward to receive the reward.");
+            }
+        }
+
 
         public string Author
         {
