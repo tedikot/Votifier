44151939537f066888125330b9f9e82a755eb79f
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-01-11 02:27:43 +0100

    Fixed Watcher set

--- VotifierConfiguration.cs
+++ VotifierConfiguration.cs
@@ -33,7 +33,7 @@ namespace unturned.ROCKS.Votifier
                 configuration.RewardBundles = new List<RewardBundle>() { 
                     new RewardBundle() { Name="Survival", Rewards = new List<Reward>() { new Reward(245, 1), new Reward(81, 2), new Reward(16, 2) }, Probability = 33 },
                     new RewardBundle() { Name="Brute Force", Rewards = new List<Reward>() { new Reward(112, 1), new Reward(113, 3), new Reward(254, 3) }, Probability = 33 },
-                    new RewardBundle() { Name="Watcher", Rewards = new List<Reward>() { new Reward(109, 1), new Reward(111, 3), new Reward(236, 3) }, Probability = 33 }
+                    new RewardBundle() { Name="Watcher", Rewards = new List<Reward>() { new Reward(109, 1), new Reward(111, 3), new Reward(236, 1) }, Probability = 33 }
                 };
                 configuration.Services = new List<Service>() { new Service("unturned-servers.net"), new Service("unturnedsl.com") };
                 return configuration;
