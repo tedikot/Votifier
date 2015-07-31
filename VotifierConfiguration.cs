29732d08efab24a8e2240aeae8724b489bb9993f
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-07-31 03:21:15 +0200

    .

--- VotifierConfiguration.cs
+++ VotifierConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Rocket.API;
 using System.Collections.Generic;
 using System.Xml.Serialization;
+using System;
 
 namespace unturned.ROCKS.Votifier
 {
@@ -8,21 +9,22 @@ namespace unturned.ROCKS.Votifier
     {
         [XmlArray("Services")]
         [XmlArrayItem(ElementName = "Service")]
-        public List<Service> Services = new List<Service>() { new Service("unturned-servers.net"), new Service("unturnedsl.com") };
-
-        public bool EnableRewardBundles = true;
+        public List<Service> Services;
+        public bool EnableRewardBundles;
 
         [XmlArray("RewardBundles")]
         [XmlArrayItem(ElementName = "RewardBundle")]
-        public List<RewardBundle> RewardBundles = new List<RewardBundle>() {
-                    new RewardBundle() { Name="Survival", Rewards = new List<Reward>() { new Reward(245, 1), new Reward(81, 2), new Reward(16, 1) }, Probability = 33 },
-                    new RewardBundle() { Name="Brute Force", Rewards = new List<Reward>() { new Reward(112, 1), new Reward(113, 3), new Reward(254, 3) }, Probability = 33 },
-                    new RewardBundle() { Name="Watcher", Rewards = new List<Reward>() { new Reward(109, 1), new Reward(111, 3), new Reward(236, 1) }, Probability = 33 }
-                };
+        public List<RewardBundle> RewardBundles;
 
         [XmlArray("ServiceDefinitions")]
         [XmlArrayItem(ElementName = "ServiceDefinition")]
-        public List<ServiceDefinition> ServiceDefinitions = new List<ServiceDefinition>() {
+        public List<ServiceDefinition> ServiceDefinitions;
+
+        public void LoadDefaults()
+        {
+            Services = new List<Service>() { new Service("unturned-servers.net"), new Service("unturnedsl.com") };
+            EnableRewardBundles = true;
+            ServiceDefinitions = new List<ServiceDefinition>() {
                     new ServiceDefinition() {
                         Name = "unturned-servers.net",
                         CheckHasVoted = "http://unturned-servers.net/api/?object=votes&element=claim&key={0}&steamid={1}",
@@ -34,6 +36,12 @@ namespace unturned.ROCKS.Votifier
                         ReportSuccess = "http://unturnedsl.com/api/dedicated/post/{0}/{1}"
                     }
                 };
+            RewardBundles = new List<RewardBundle>() {
+                new RewardBundle() { Name="Survival", Rewards = new List<Reward>() { new Reward(245, 1), new Reward(81, 2), new Reward(16, 1) }, Probability = 33 },
+                new RewardBundle() { Name="Brute Force", Rewards = new List<Reward>() { new Reward(112, 1), new Reward(113, 3), new Reward(254, 3) }, Probability = 33 },
+                new RewardBundle() { Name="Watcher", Rewards = new List<Reward>() { new Reward(109, 1), new Reward(111, 3), new Reward(236, 1) }, Probability = 33 }
+            };
+        }
     }
 
     public class RewardBundle
